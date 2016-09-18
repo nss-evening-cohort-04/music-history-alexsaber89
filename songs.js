156857068e87538a759373984b4f121437a2a64f
Alex Saber <Alex Saber>
2016-09-18 16:51:19 -0500

    user can now enter their desired song, artist, and album, which will then be added to the list view

--- songs.js
+++ songs.js
@@ -8,9 +8,23 @@ function activateAddView() {
   document.getElementById("list-music-view").style.display = "none";
 }
 
+function collectUserInput() {
+  var userInputSongName = document.getElementById("song-name");
+  var userInputArtist = document.getElementById("artist");
+  var userInputAlbum = document.getElementById("album");
+  var newSongString = userInputSongName.value + " - by " + userInputArtist.value + " on the album " + userInputAlbum.value;
+  songs.push(newSongString);
+  songListDiv.innerHTML += "<p>" + newSongString + "</p>";
+  userInputSongName.value = "";
+  userInputArtist.value = "";
+  userInputAlbum.value = "";
+  activateListView();
+}
+
 activateListView();
 document.getElementById("list-view-btn").addEventListener("click", activateListView);
 document.getElementById("add-view-btn").addEventListener("click", activateAddView);
+document.getElementById("add-btn").addEventListener("click", collectUserInput);
 var songs = [];
 
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
