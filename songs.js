df5a57a27bcd0377e0946b6287c0a6cdb010ca60
Alex Saber <Alex Saber>
2016-09-20 18:01:38 -0500

    json now populating on page

--- songs.js
+++ songs.js
@@ -21,20 +21,44 @@ function collectUserInput() {
   activateListView();
 }
 
+function parseJSON() {
+  var data = JSON.parse(this.responseText);
+  songObjects = data.songList;
+  populateSongDiv(songObjects);
+}
+
+function populateSongDiv(array) {
+  for (var i = 0; i < array.length; i++) {
+    songListDiv.innerHTML += "<p>" + array[i].song_name + " by " + array[i].artist_name + " on the album " + array[i].album_name + "</p>";
+  }
+}
+
+//On page load, activate the list view
 activateListView();
 document.getElementById("list-view-btn").addEventListener("click", activateListView);
 document.getElementById("add-view-btn").addEventListener("click", activateAddView);
 document.getElementById("add-btn").addEventListener("click", collectUserInput);
 
-var songs = [];
+var songObjects = [];
 var songListDiv = document.getElementById("song_list_id");
 
-for (var i = 0; i < songs.length; i++) {
-  songs[i] = songs[i].replace(/\*/g, "");
-  songs[i] = songs[i].replace(/\(/g, "");
-  songs[i] = songs[i].replace(/@/g,"");
-  songs[i] = songs[i].replace(/!/g,"");
-  songs[i] = songs[i].replace(/>/g," - ");
-  console.log(songs[i]);
-  songListDiv.innerHTML += "<p>" + songs[i] + "</p>";
-}
+
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener("load", parseJSON);
+myRequest.open("GET", "songList.json");
+myRequest.send();
+
+// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+// songs.unshift("Postpartum - by Taylor McFerrin on the album Early Riser");
+// songs.push("Perpetual Black Second - by Meshuggah on the album Nothing");
+
+  // songs[i] = songs[i].replace(/\*/g, "");
+  // songs[i] = songs[i].replace(/\(/g, "");
+  // songs[i] = songs[i].replace(/@/g,"");
+  // songs[i] = songs[i].replace(/!/g,"");
+  // songs[i] = songs[i].replace(/>/g," - ");
